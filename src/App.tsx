03f8b7c7931aa07e13e9361aeaa7d35fbd8cd355
IsaacBarros90 <andresantoas@gmail.com>
2026-05-19 15:16:40 -0300

    CSS novo

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,56 @@
 import './App.css'
-
+import Roxa from './assets/Roxa.png'
 
 function App() {
 
   return (
     <>
-      <head>
-        <title>FocoQuest</title>
-      </head>
       <header className="Nav">
         <div className="nav-box">
           <div className='header-left'>
-            <img src="" alt="" className='Logo' />
-            <h1>FocoQuest</h1>
-            <p>Motive-se a alcançar <br /> seus objetivos.</p>
+            <img src={Roxa} className='Logo'/>
+              <div className='header-text'>
+                <h1>FOCOQUEST</h1>
+                <p>Motive-se a alcançar <br /> seus objetivos.</p>
+              </div>
+            </div>
+            <div className='Login'>
+              <h2>INICIAR SESSÃO</h2>
+              <img src="https://cdn-icons-png.flaticon.com/128/2102/2102633.png"/>
+            </div>
           </div>
-          <div className='Login'>
-            <h2>Iniciar Sessão</h2>
-            <img src="https://cdn-icons-png.flaticon.com/128/2102/2102633.png" alt="" />
-          </div>
-        </div>
         </header>
+
         <section id='Home'>
           <article className='triangle'>
             <p>Ganhe equipamentos, mascotes e habilidades mágicas conforme avança.</p>
           </article>
-          <article className='img'>
-            <p><img src="" alt="" /><br />SE DESAFIE</p>
+          <article className='card'>
+            <img src="https://i.imgur.com/lYqXWJx.png"/>
+            <h3>SE DESAFIE</h3>
           </article>
           <article className='triangle'>
             <p>Complete tarefas para ganhar experiência e subir de nível com seu personagem.</p>
           </article>
-          <article className='img'>
-            <p><img src="" alt="" /><br />CRIE SEU PERSONAGEM</p>
+          <article className='card'>
+            <img src="https://i.imgur.com/F7g8z4x.png"/>
+            <h3>CRIE SEU PERSONAGEM</h3>
           </article>
           <article className='triangle'>
             <p>Progrida nas metas para liberar desafios e missões especiais.</p>
           </article>
         </section>
-        <aside>
-          <img src="" alt="" />
-        </aside>
-        <footer>
-            <img src="" alt="" className='Logo' />
-            <p>Junte-se a milhões de pessoas que organizam trabalho e vida pessoal com FocoQuest</p>
 
+        <aside>
+          <img src="https://i.imgur.com/lYqXWJx.png" className='banner' />
+        </aside>
+
+        <footer>
+          <div className='footer-top'>
+            <img src={Roxa} className='footer-logo' />
+            <p>Junte-se a milhões de pessoas que organizam trabalho e vida pessoal com FocoQuest</p>
+          </div>
+          <div className='footer-links'>
             <div>
               <h3>Produto</h3>
               <p>Como funciona</p>
@@ -65,6 +71,14 @@ function App() {
               <p>Reportar um problema</p>
               <p>Solicitar Sugestão</p>
             </div>
+          </div>
+          <div className='footer-bottom'>
+            <p>© 8088 FocoQuest. Todos os direitos reservados.</p>
+            <div>
+              <span>Política de Privacidade</span>
+              <span>Termos de Uso</span>
+            </div>
+          </div>
         </footer>
     </>
   )
