8e409b796dc9739603b288311d12972c6c549a82
IsaacBarros90 <andresantoas@gmail.com>
2026-09-21 22:45:57 -0300

    Api att

--- api.ts
+++ api.ts
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:6900';
+export const API_URL = import.meta.env.VITE_API_URL ?? 'https://backend-novo.onrender.com';
 
 export const publicApiHeaders: HeadersInit = {
 	'bypass-tunnel-reminder': 'true',
